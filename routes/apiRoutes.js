038048d5a71f5189b7779fb42fed6841e9a26d5e
Caroline Tobin <ctobin25>
2021-03-20 09:47:47 -0400

    pushed notes to dom

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,61 +1,90 @@
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
-module.exports = function(app) {
+module.exports = function (app) {
     console.log("hit")
-    app.get("/api/notes", function(req, res) {
+    app.get("/api/notes", function (req, res) {
         fs.readFile('db/db.json', (err, data) => {
             if (err) {
-              console.error(err)
-              return
+                console.error(err)
+                return
             }
-           // console.log(data)
+            // console.log(data)
             var obj = JSON.parse(data);
             console.log(obj)
 
             res.json(obj);
         })
-     
-      });
 
-     
-  app.post("/api/notes", function(req, res) {
-
-    const uuid = uuidv1();
-   
-    const newNote = req.body;
- 
-    newNote.id = uuid;
-
-   
-    let db = readDb;
-
-    db.push(newNote); 
-
- 
-    writeDb(db);
-
-   
-    res.json(db);
-  });
+    });
 
 
-  app.delete("/api/notes/:id", function(req, res) {
-    
-    const db = readDb;
-    
-    const deleteId = req.params.id;
+    app.post("/api/notes", function (req, res) {
 
-    for (let i=0; i<db.length; i+=1 ) {
-      if (db[i].id === deleteId) {
+        const uuid = uuidv4();
+        // console.log(uuid)
+
+        const newNote = req.body;
         
-        db.splice(i,1);
-       
-        break;
-      }
+
+        newNote.id = uuid;
+         console.log(newNote)
+
+        fs.readFile('db/db.json', (err, data) => {
+            if (err) {
+                console.error(err)
+                return
+            }
+            var dbNotes = JSON.parse(data);
+            console.log(dbNotes)
+
+            dbNotes.push(newNote)
+            console.log(dbNotes)
+            var myJSON = JSON.stringify(dbNotes);
+
+            fs.writeFile("db/db.json", myJSON, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+                console.log("The file was saved!");
+
+                res.json(dbNotes);
+            }); 
+
+
+
+            
+        })
+
+        // let db = readDb;
+
+        // db.push(newNote);
+
+
+        // writeDb(db);
+
+
+        // res.json(db);
+    });
+
+
+    // app.delete("/api/notes/:id", function (req, res) {
+
+    //     const db = readDb;
+
+    //     const deleteId = req.params.id;
+
+    //     for (let i = 0; i < db.length; i += 1) {
+    //         if (db[i].id === deleteId) {
+
+    //             db.splice(i, 1);
+
+    //             break;
+    //         }
 
 
 
 
 
 
-}
+        }
