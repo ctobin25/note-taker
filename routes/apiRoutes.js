0634505d099ee632bfd6f5469e4edeb9dbdba9a9
Caroline Tobin <ctobin25>
2021-03-20 09:02:49 -0400

    api routes added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -17,4 +17,45 @@ module.exports = function(app) {
      
       });
 
+     
+  app.post("/api/notes", function(req, res) {
+
+    const uuid = uuidv1();
+   
+    const newNote = req.body;
+ 
+    newNote.id = uuid;
+
+   
+    let db = readDb;
+
+    db.push(newNote); 
+
+ 
+    writeDb(db);
+
+   
+    res.json(db);
+  });
+
+
+  app.delete("/api/notes/:id", function(req, res) {
+    
+    const db = readDb;
+    
+    const deleteId = req.params.id;
+
+    for (let i=0; i<db.length; i+=1 ) {
+      if (db[i].id === deleteId) {
+        
+        db.splice(i,1);
+       
+        break;
+      }
+
+
+
+
+
+
 }
